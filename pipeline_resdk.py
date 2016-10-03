8ee03d43947d6168b54324d2f411a0c9d1d6bf0c
charlesylin <charles.y.lin@gmail.com>
2016-10-03 16:31:27 -0500

    added more relationship table functions

--- pipeline_resdk.py
+++ pipeline_resdk.py
@@ -57,7 +57,7 @@ projectFolder = '/grail/projects/chordoma/'
 #===========================DEFINING THE CLASSES=================================
 #================================================================================
 
-collection = res.collection.get(collection_slug)
+
 
 
 #================================================================================
@@ -66,11 +66,13 @@ collection = res.collection.get(collection_slug)
 
 #user defined classes her
 
-class DataDict:
+class ResCollection:
 
     #this __init__section is called from the get go
-    def __init__(self,collection,relationship_file=''):
-        print('Loading collection %s' % (collection.slug))
+    def __init__(self,collection_slug,relationship_file=''):
+        
+        print('Loading collection %s' % (collection_slug))
+        collection = res.collection.get(collection_slug)        
         self._collection = collection
 
         sample_dict = {}
@@ -105,11 +107,29 @@ class DataDict:
         #now establish a relationship dictionary for groups and sample relationships
         self._background_dict = {}
         for name in sample_names:
-            self._background_dict[name]=''
+            self._background_dict[name]='NONE'
 
+        #now establish a relationship dictionary for groups and sample relationships
+        self._group_dict = {}
+        for name in sample_names:
+            self._group_dict[name]='NONE'
+
+        #if a relationship table is provided, import it.
+        #if not, create a basic table
         
+        if len(relationship_file) > 0: # something provided by user
+            #first step is to see if it exists
+            try:
+                f = open(relationship_file,'r')
+                f.close()
+                self.importRelationships(relationship_file)
+            except IOError: #nothing present
+                self.exportRelationships(relationship_file)
+
     def exportRelationships(self,output=''):
-        
+        print('Creating relationship table')
+        if len(output) >0:
+            print('Writing relationships to %s' % (output))
         rel_table = [['SAMPLE_NAME','SAMPLE_SLUG','U_ID','BACKGROUND_NAME','GROUP']]
 
         for name in self._names:            
@@ -127,18 +147,19 @@ class DataDict:
 
 
     def importRelationships(self,input_table):
-        background_dict={}
-        rel_table = [['SAMPLE_NAME','SAMPLE_SLUG','U_ID','BACKGROUND_NAME','GROUP']]
-        filename=input_table
-        with open(input_table) as f:
-            for line in f:
-                sample.name,sample.slug,sample.id,background.name,group=f.split("\t")
-                background_dict[sample.name]={}
-                background_dict[sample.name]['unique_id']=sample.id #returns unique id
-                background_dict[sample.name]['slug']=sample.slug #returns slug
-                background_dict[sample.name]['background']=background.name #returns background name
-                background_dict[sample.name]['group']=group #returns group
+        print('Importing relationship data from %s' % (input_table))
 
+        rel_table = [['SAMPLE_NAME','SAMPLE_SLUG','U_ID','BACKGROUND_NAME','GROUP']]
+        
+        f = open(input_table,'r')
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip().split('\t')
+            name = line[0]
+            background = line[3]
+            group = line[4]
+            self._background_dict[name]=background
+            self._group_dict[name]=group
 
 
     def setBackground(self,name,background_name):
@@ -153,9 +174,25 @@ class DataDict:
 
         self._background_dict[name] = background_name
 
+
+    def setGroup(self,name,group_name):
+
+        #take in a sample name and a background name and set the background dict
+        
+        #check that each exist
+        if self._names.count(name) == 0:
+            print('ERROR: %s not in collection' % (name))
+
+        self._group_dict[name] = group_name
+
     def names(self):
         #returns all sample names
         return self._names
+
+    def group(self,name):
+        #returns all sample names
+        return self._group_dict[name]
+
 
     def getBamID(self,name):
 
@@ -164,7 +201,11 @@ class DataDict:
 
     def getBackground(self,name):
         #return the sample name of the background!!!
-        return self._background_dict[name]
+        background_name = self._background_dict[name]
+        if background_name == 'NONE' or background_name == '':
+            return None
+        else:
+            return self._background_dict[name]
 
 
 
@@ -228,31 +269,23 @@ def main():
     this is the main run function for the script
     all of the work should occur here, but no functions should be defined here
     '''
-
-    # sample_dict = make_sample_dict(collection)
-
-    # print(sample_dict)
-
-    # bam_id = get_bam('PRIMARY_CHOR_142A2_H3K27AC',sample_dict)
-
-    # print(bam_id)
-
-
-    #my_name=sample_dict.keys()[1]
+    projectFolder = '/grail/projects/pipeline_resdk/'
     
-    #my_bam=get_bam(my_name,sample_dict)
+    #ideal situation
+    res_collection = ResCollection(collection_slug,'%sfoo.txt' % (projectFolder)) #if foo exists, load it, if not write it out to disk
+
+    #all of the datasets that we have
+    #names_list = res_collection.names()
     
-    #load the dataDict object
-    dataDict = DataDict(collection)
+    #only want k27ac datasets
+    names_list = [name for name in res_collection.names() if res_collection.group(name) == 'H3K27AC']
+    for name in names_list:
+        background_name = res_collection.getBackground(name)
+        if background_name:
+            bamID = res_collection.getBamID(background_name)
+            print('For dataset %s, the background is %s and the bamID for the background is %s' % (name,background_name,bamID))
+        else:
+            print('For dataset %s, No background was found' % (name))
 
-    dataDict.exportRelationships('/home/rociod/src/foo.txt')
-    dataDict.importRelationships('/home/rociod/src/foo.2.txt')
-
-
-    # print(dataDict.names())
-
-    # print(dataDict.getBam('PRIMARY_CHOR_142A2A4_H3K27AC'))
-
-    
 main()
 
